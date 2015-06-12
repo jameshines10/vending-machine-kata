2fcadbc7e35d3dddb2aca70a0b98c5454a59b3c6
James Hines <jameshines10@gmail.com>
2015-06-12 19:04:49 -0400

    Refactors the VendingMachine class to dynamically identify a coin and assign it a value

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,9 +1,29 @@
+function identifyCoin(coin) {	
+	if(coin.weight === 5 && coin.diameter === 21.21 && coin.thickness === 1.95) {
+		return 'nickel';
+	}	 
+}
+
+function assignValue(coinType) {
+	var coinValue = 0;
+
+	switch(coinType) {
+		case 'nickel':
+			coinValue = 5;
+			break;
+	}
+
+	return coinValue;
+}
+
 var VendingMachine = function(initialDeposit) {
 	this.currentAmount = initialDeposit;
 };
 
 VendingMachine.prototype.acceptCoin = function(coin) {
-	this.currentAmount += 5;
+	var coinType = identifyCoin(coin);
+
+	this.currentAmount += assignValue(coinType);
 };
 
 VendingMachine.prototype.getCurrentAmount = function() {
--- test/vending-machine.spec.js
+++ test/vending-machine.spec.js
@@ -2,7 +2,7 @@ describe('VendingMachine', function() {
 	var vendingMachine = new VendingMachine(1000);
 
     it('should accept coins', function() {
-    	vendingMachine.acceptCoin(Coin.Nickel);
+    	vendingMachine.acceptCoin(Coin.NICKEL);
 
         expect(vendingMachine.getCurrentAmount()).toEqual(1005);
     });    
