11d8bd167ffa9ebd03ee1646b150e20854310de1
James Hines <jameshines10@gmail.com>
2015-06-23 07:47:55 -0400

    Removes the uneeded stubs for the Coin object

--- test/vending-machine.spec.js
+++ test/vending-machine.spec.js
@@ -4,35 +4,10 @@ describe('VendingMachine', function() {
 
 	beforeEach(function(){
 		vendingMachine = new VMK.VendingMachine(1000);
-
-		VMK.Coin = {
-			NICKEL: {
-				weight: 5, // in grams
-				diameter: 21.21, // in mm
-				thickness: 1.95 // in mm
-			},
-			DIME: {
-				weight: 2.268,
-				diameter: 17.91,
-				thickness: 1.35
-			},
-			QUARTER: {
-				weight: 5.670,
-				diameter: 24.26,
-				thickness: 1.75
-			},
-			PENNY: {
-				weight: 2.500,
-				diameter: 19.05,
-				thickness: 1.52
-			}
-		}
-
-		nickel = VMK.Coin.NICKEL;
 	});
 
     it('should accept nickels', function() {
-    	vendingMachine.acceptCoin(nickel);
+    	vendingMachine.acceptCoin(VMK.Coin.NICKEL);
 
         expect(vendingMachine.displayCurrentAmount()).toEqual(5);
     });
